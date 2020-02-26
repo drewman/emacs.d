5beac5a2b58b7609920479414934777df3487860
Drew Bruce <drew.bruce@nike.com>
2020-02-26 12:30:26 -0800

    reoganize keybinds

--- init.el
+++ init.el
@@ -431,7 +431,9 @@
 (evil-define-key 'insert global-map (kbd "C-<SPC>") my/leader-map)
 (evil-define-key 'visual global-map (kbd "C-<SPC>") my/leader-map)
 
+(which-key-add-key-based-replacements "<SPC> c" "open config")
 (define-key my/leader-map "c" '(lambda () (interactive) (find-file "~/.emacs.d/init.el")))
+
 (define-key my/leader-map "f" 'counsel-find-file)
 (define-key my/leader-map "x" 'counsel-M-x)
 (define-key my/leader-map "p" 'projectile-command-map)
@@ -445,6 +447,8 @@
 (define-key my/leader-map "u" 'ivy-switch-buffer)
 ;; (define-key my/leader-map "w" 'widen)
 (define-key my/leader-map "r" 'ranger)
+(define-key my/leader-map "t" 'shell-pop)
+(define-key my/leader-map "D" 'counsel-descbinds)
 
 (which-key-add-key-based-replacements "<SPC> g" "magit-prefix")
 (define-key my/leader-map "gs" 'magit-status)
@@ -452,37 +456,33 @@
 (define-key my/leader-map "gl" 'magit-log)
 (define-key my/leader-map "gb" 'magit-branch-and-checkout)
 
+(which-key-add-key-based-replacements "<SPC> o" "org-prefix")
 (define-key my/leader-map "oa" 'org-agenda)
 (define-key my/leader-map "ol" 'org-store-link)
 (define-key my/leader-map "or" 'org-refile)
 (define-key my/leader-map "ox" 'org-export-dispatch)
 (define-key my/leader-map "oc" 'org-confluence-export-as-confluence)
+
+(which-key-add-key-based-replacements "<SPC> oe" "emacs notes")
 (define-key my/leader-map "oe" '(lambda () (interactive) (find-file "~/org/emacs-notes.org")))
+
+(which-key-add-key-based-replacements "<SPC> op" "personal notes")
 (define-key my/leader-map "op" '(lambda () (interactive) (find-file "~/org/personal-notes.org")))
+
+(which-key-add-key-based-replacements "<SPC> ow" "work notes")
 (define-key my/leader-map "ow" '(lambda () (interactive) (find-file "~/org/nike-work-notes.org")))
+
+(which-key-add-key-based-replacements "<SPC> oi" "inbox")
 (define-key my/leader-map "oi" '(lambda () (interactive) (find-file "~/org/inbox.org")))
 
+(which-key-add-key-based-replacements "<SPC> w" "window-prefix")
 (define-key my/leader-map "wo" 'other-window)
 (define-key my/leader-map "wv" 'split-window-vertically)
 (define-key my/leader-map "wh" 'split-window-horizontally)
 (define-key my/leader-map "wq" 'delete-window)
 
-(which-key-add-key-based-replacements "<SPC> o" "org-prefix")
-(which-key-add-key-based-replacements "<SPC> w" "window-prefix")
-(which-key-add-key-based-replacements "<SPC> c" "open config")
-(which-key-add-key-based-replacements "<SPC> oe" "emacs notes")
-(which-key-add-key-based-replacements "<SPC> op" "personal notes")
-(which-key-add-key-based-replacements "<SPC> ow" "work notes")
-(which-key-add-key-based-replacements "<SPC> oi" "inbox")
-
-(define-key my/leader-map "D" 'counsel-descbinds)
-
-(define-key my/leader-map "yn" 'yas-new-snippet)
-(define-key my/leader-map "ye" 'yas-expand)
-
 (which-key-add-key-based-replacements "<SPC> y" "yas-prefix")
-
-(define-key my/leader-map "t" 'shell-pop)
+(define-key my/leader-map "yn" 'yas-new-snippet)
 
 ;;(evil-global-set-key 'motion "C-," 'er/expand-region)
 (evil-global-set-key 'normal "/" 'swiper-isearch)
